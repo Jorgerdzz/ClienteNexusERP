8738678686b057e0b0f8ade40dade7251afb7ba7
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-05-06 14:06:16 +0200

    Correción actualización perfil usuario

--- NexusERP/Controllers/ProfileController.cs
+++ NexusERP/Controllers/ProfileController.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using ApiNexusERP.DTOs;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
-using NexusERP.Extensions;
-using NexusERP.Models;
 using NexusERP.Services;
 using NexusERP.ViewModels;
 using System.Security.Claims;
-using ApiNexusERP.DTOs;
-using System.Threading.Tasks;
 
 namespace NexusERP.Controllers
 {
@@ -34,7 +31,7 @@ namespace NexusERP.Controllers
                 NombreEmpresa = "Empresa Actual", // Valor extraído de otro origen si es necesario
                 EstaVinculadoAEmpleado = user.EmpleadoId.HasValue
             };
-            if(model.EstaVinculadoAEmpleado && user.Empleado != null)
+            if (model.EstaVinculadoAEmpleado && user.Empleado != null)
             {
                 model.EmpleadoNombreCompleto = $"{user.Empleado.Nombre} {user.Empleado.Apellidos}";
                 model.EmpleadoDNI = user.Empleado.Dni;
@@ -59,21 +56,38 @@ namespace NexusERP.Controllers
                 return RedirectToAction("Index");
             }
 
-            var usuarioActualizado = await this.serviceUsuarios.UpdatePerfilUsuarioAsync(model.UsuarioId, model.NombreUsuario, model.Email);
+            string idUsuarioString = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int idUsuarioReal = int.Parse(idUsuarioString);
+
+            var usuarioActualizado = await this.serviceUsuarios.UpdatePerfilUsuarioAsync(idUsuarioReal, model.NombreUsuario, model.Email);
 
             if (usuarioActualizado != null)
             {
-  
                 var identity = (ClaimsIdentity)User.Identity;
 
                 var claimNombreAntiguo = identity.FindFirst(ClaimTypes.Name);
                 var claimEmailAntiguo = identity.FindFirst(ClaimTypes.Email);
+                var claimInicialesAntiguo = identity.FindFirst("Iniciales");
 
                 if (claimNombreAntiguo != null) identity.RemoveClaim(claimNombreAntiguo);
                 if (claimEmailAntiguo != null) identity.RemoveClaim(claimEmailAntiguo);
+                if (claimInicialesAntiguo != null) identity.RemoveClaim(claimInicialesAntiguo);
 
+                // Añadimos los nuevos datos básicos
                 identity.AddClaim(new Claim(ClaimTypes.Name, model.NombreUsuario));
                 identity.AddClaim(new Claim(ClaimTypes.Email, model.Email));
+
+                // 🎨 RECALCULAR LAS INICIALES PARA EL AVATAR
+                string iniciales = "US";
+                if (!string.IsNullOrWhiteSpace(model.NombreUsuario))
+                {
+                    var partesNombre = model.NombreUsuario.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    if (partesNombre.Length >= 2)
+                        iniciales = $"{partesNombre[0][0]}{partesNombre[1][0]}".ToUpper();
+                    else if (partesNombre.Length == 1)
+                        iniciales = partesNombre[0].Substring(0, Math.Min(2, partesNombre[0].Length)).ToUpper();
+                }
+                identity.AddClaim(new Claim("Iniciales", iniciales));
 
                 await HttpContext.SignInAsync(
                     CookieAuthenticationDefaults.AuthenticationScheme,
